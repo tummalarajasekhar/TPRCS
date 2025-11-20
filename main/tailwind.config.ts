a3cbe53011a7183c627a01861451d9434e4a8b01
tummalarajasekhar <trsr.rajasekhar@gmail.com>
2025-11-20 23:52:01 +0530

    mern -training -2

--- main/tailwind.config.ts
+++ main/tailwind.config.ts
@@ -85,10 +85,7 @@ const config: Config = {
           },
         },
       },
-      animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
-      },
+     
     },
   },
   plugins: [require('tailwindcss-animate')],
