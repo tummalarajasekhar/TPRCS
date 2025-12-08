db897fbba47b4433b6e327765227565ec21f3c5a
tummalarajasekhar <trsr.rajasekhar@gmail.com>
2025-12-08 16:34:42 +0530

    config

--- main/next.config.ts
+++ main/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // This tells Next.js to ignore ESLint errors during the build
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // This tells Next.js to ignore TypeScript errors during the build
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
